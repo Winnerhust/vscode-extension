ac19635decf1f69a04e80a1aec1f421d79e94eaf
WinnerHust <910894904@qq.com>
2018-08-03 00:57:12 +0800

    add referencedocument

--- lookupreferences/src/extension.ts
+++ lookupreferences/src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import ContentProvider, { encodeLocation } from './provider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,17 +12,31 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "lookupreferences" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+	const provider = new ContentProvider();
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+	// register content provider for scheme `references`
+	// register document link provider for scheme `references`
+	const providerRegistrations = vscode.Disposable.from(
+		vscode.workspace.registerTextDocumentContentProvider(ContentProvider.scheme, provider),
+		vscode.languages.registerDocumentLinkProvider({ scheme: ContentProvider.scheme }, provider),
+	);
 
-    context.subscriptions.push(disposable);
+	// register command that crafts an uri with the `references` scheme,
+	// open the dynamic document, and shows it in the next editor
+	const commandRegistration = vscode.commands.registerTextEditorCommand('editor.printReferences', editor => {
+		const uri = encodeLocation(editor.document.uri, editor.selection.active);
+		return vscode.workspace.openTextDocument(uri).then(doc => {
+            if(editor !== undefined && editor.viewColumn !== undefined){
+                vscode.window.showTextDocument(doc, editor.viewColumn + 1);
+            }
+        });
+	});
+
+	context.subscriptions.push(
+		provider,
+		commandRegistration,
+		providerRegistrations
+	);
 }
 
 // this method is called when your extension is deactivated
